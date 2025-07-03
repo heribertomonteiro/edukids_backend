45f39b21ad5d25a6b1c1f1286180af0e5320d8d4
Heriberto Costa Monteiro <heribertoomonteiroo@gmail.com>
2025-07-03 14:14:08 -0300

    chore: adição da primeira view

--- edukids/settings.py
+++ edukids/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -114,9 +115,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Diretórios adicionais onde o Django deve procurar arquivos estáticos
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
